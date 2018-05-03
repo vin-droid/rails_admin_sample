3235d56cdadeb50a2439cd352b53f6b11bf72880
Vineet Sahu <vineet.12cs069@abes.ac.in>
2018-05-03 18:58:03 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  get 'home/index'
+
+  root to: "home#index"
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
